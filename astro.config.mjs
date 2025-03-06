2912935cd6500f12447baa813a369b8b0a7d0cd9
EnterNathan <nathancarqueijeiroetd@gmail.com>
2025-03-06 08:25:42 +0100

    modif + ajout de descriptions et deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site:'https://EnterNAthan.github.io',
-  base:'/',
+  base:'NathFolio',
   trailingSlash: 'always',
   integrations: [tailwind(), react()],
   vite: {
